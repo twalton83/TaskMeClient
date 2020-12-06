8037b2a8ec5ee391b88315b93d6b6eafd73dcf5b
Tatiana <tatianaw83@gmail.com>
2020-12-05 21:09:00 -0800

    Initialized CRA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Switch, Route } from 'react-router-dom'
+import './App.css'
+import HomeView from './components/HomeView'
+import Sidebar from './components/Sidebar'
+import ProjectsList from './components/ProjectsList'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      <Switch>
+        <Route exact to="/">
+          <HomeView />
+        </Route>
+        <Route exact to="/projects">
+          <ProjectsList />
+        </Route>
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
